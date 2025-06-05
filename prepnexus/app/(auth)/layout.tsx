c3d154add986a96c8cfd66b5941ee990dc3ab41c
riyaaryan50 <riyaaryan50@gmail.com>
2025-06-06 00:48:27 +0530

    created routes

---
+++ prepnexus/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AuthLayout = ()=>{
+return(
+  <div>Layout</div>
+)
+}
+
+export default AuthLayout; 
---
+++ prepnexus/app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { Children, ReactNode } from "react";
+
+const RootLayout = ({children}:{children : ReactNode})=>{
+return(
+  <div>{children}</div>
+)
+}
+
+export default RootLayout; 
