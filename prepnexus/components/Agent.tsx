c03a11f357a9909d076b70cd85937f7a3084697a
riyaaryan50 <riyaaryan50@gmail.com>
2025-06-08 01:16:52 +0530

    Update Agent.tsx

--- prepnexus/components/Agent.tsx
+++ prepnexus/components/Agent.tsx
@@ -1,8 +1,25 @@
 import   React from 'react'
 
-const Agentt = () => {
+const Agentt = ({userName} : AgentProps) => {
+  const isSpeaking = true;
   return (
-    <div>Agent</div>
+    <>
+      <div className="call=view">
+      <div className="card-interviewer">
+        <div className="avatar">
+          <Image src="/ai-avatar.png" alt="vapi" width={65} height={54} className="object-cover" />
+          {isSpeaking && <span className="animate-speak" />}
+        </div>
+        <h3>AI Interviewer</h3>
+      </div>
+      <div className="card-border">
+        <div className="card-content">
+          <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className="rounded-full object-cover size=[120px]" />
+          <h3>{userName}</h3>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 export default Agent
