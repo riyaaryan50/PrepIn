7b895d21389e2e30c7a1497d9517d1530d0ee507
riyaaryan50 <riyaaryan50@gmail.com>
2025-06-08 11:29:52 +0530

    Update layout.tsx

--- prepnexus/app/(auth)/layout.tsx
+++ prepnexus/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import {ReactNode}from "react";
 import { redirect } from "next/navigation";
-import { isAuthenticated } from "@/lib/auth.action";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 
 const AuthLayout = async ({children}:{children:ReactNode})=>{
