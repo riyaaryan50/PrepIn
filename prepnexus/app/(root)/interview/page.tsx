49ae7aeb5d7110835d0a5a21e58d2f2352d37773
riyaaryan50 <riyaaryan50@gmail.com>
2025-06-08 11:16:31 +0530

    Update page.tsx

--- prepnexus/app/(root)/interview/page.tsx
+++ prepnexus/app/(root)/interview/page.tsx
@@ -1,5 +1,5 @@
 import Agent from "@/components/Agent";
-import { getCurrentUser } from "@/lib/actions/auth.action";
+import { getCurrentUser } from "@/lib/auth.action";
 
 const Page = async () => {
   const user = await getCurrentUser();
